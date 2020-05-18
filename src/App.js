3028f61a5b0d84bd7d984d94252c602c7129a29e
dfjung4254 <rmsghk4254@naver.com>
2020-05-19 02:24:14 +0900

    React tutorial studying

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import PhoneForm from './components/PhoneForm';
 import './App.css';
+import PhoneInfoList from './components/PhoneInfoList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  id = 3;
+  state = {
+    information : [
+      {
+        id : 1,
+        name : '정근화',
+        phone : '01048518057'
+      },
+      {
+        id : 2,
+        name : '홍소희',
+        phone : '01022372752'
+      }
+    ]
+  }
+
+  handleCreate = (data) => {
+    const {information} = this.state;
+    this.setState({
+      information : information.concat({
+        id : this.id++,
+        name : data.name,
+        phone : data.phone
+      })
+    });
+  }
+
+  render(){
+    return(
+      <div>
+        <PhoneForm
+          OnCreateProps={this.handleCreate}
+        />
+        <PhoneInfoList data={this.state.information} />
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/components/PhoneForm.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+
+class PhoneForm extends Component {
+
+  /* state 선언 */
+  state = {
+    name : '',
+    phone : ''
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name] : event.target.value
+    });
+  }
+
+  handleSubmit = (event) => {
+    /* 페이지 리로딩 방지 */
+    event.preventDefault();
+    this.props.OnCreateProps(this.state);
+    this.setState({
+      name : '',
+      phone : ''
+    });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          placeholder="이름"
+          value={this.state.name}
+          onChange={this.handleChange}
+          name="name"
+        />
+        <input
+          placeholder="전화번호"
+          value={this.state.phone}
+          onChange={this.handleChange}
+          name="phone"
+        />
+        <button type="submit">등록</button>
+        <div>
+          {this.state.name} / {this.state.phone}
+        </div>
+      </form>
+    );
+  }
+
+}
+
+export default PhoneForm;
