e20a85f367f2c421bf9c6eaf5366711c94cd5b32
dfjung4254 <rmsghk4254@naver.com>
2020-05-22 02:04:42 +0900

    glow 작업 중 및 노트 등 3D 컴포넌트 분리작업 진행

--- src/game_components/object/cube_sample_object.js
+++ src/game_components/object/cube_sample_object.js
@@ -3,39 +3,23 @@ import * as THREE from 'three';
 class CubeSampleObject extends THREE.Mesh {
 
   constructor(){
-    const geometry = new THREE.BoxGeometry(15, 8, 2)
+    const params = {
+      exposure: 1,
+      bloomStrength: 1.5,
+      bloomThreshold: 0,
+      bloomRadius: 0
+    };
+    const geometry = new THREE.BoxGeometry(30, 6, 4)
     const material = new THREE.ShaderMaterial({
-      uniforms: {
-        color1: {
-          value: new THREE.Color("red")
-        },
-        color2: {
-          value: new THREE.Color("purple")
-        }
-      },
-      vertexShader: `
-        varying vec2 vUv;
-    
-        void main() {
-          vUv = uv;
-          gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
-        }
-      `,
-      fragmentShader: `
-        uniform vec3 color1;
-        uniform vec3 color2;
       
-        varying vec2 vUv;
-        
-        void main() {
-          
-          gl_FragColor = vec4(mix(color1, color2, vUv.y), 1.0);
-        }
-      `,
-      wireframe: true
+    
+
     });
+
     super(geometry, material);
   }
+
+
 
   animate = () => {
     // this.rotation.x += 0.1;
--- src/game_components/scene/main_scene.js
+++ src/game_components/scene/main_scene.js
@@ -16,10 +16,12 @@ class MainScene extends Component {
     const camera = new THREE.PerspectiveCamera(50, width / height, 0.1, 2000)
     camera.angle = 0;
     camera.radius = 200;
+    camera.add(new THREE.PointLight(0xffffff, 1));
     camera.animate = () => {
       var speed = Date.now() * 0.00050;
       camera.position.x = Math.cos(speed) * camera.radius;
       camera.position.z = Math.sin(speed) * camera.radius;
+      // camera.position.z = camera.radius;
       camera.lookAt(0,0,0);
     }
     sceneManager.addObject(camera);
@@ -31,13 +33,15 @@ class MainScene extends Component {
     renderer.setSize(width, height)
 
 
-
+    /* Add Objects on SceneManager - 곧 분리해야함, 렌더링 테스트용 */
     sceneManager.addObject(new CubeSampleObject());
     const anotherCube = new CubeSampleObject();
     anotherCube.position.x = 50;
     sceneManager.addObject(anotherCube);
-
     sceneManager.addObject(new GalaxySampleObject());
+    const ambientLight = new THREE.AmbientLight(0x404040);
+    ambientLight.animate = () => {}
+    sceneManager.addObject(ambientLight);
 
     this.sceneManager = sceneManager;
     this.camera = camera
