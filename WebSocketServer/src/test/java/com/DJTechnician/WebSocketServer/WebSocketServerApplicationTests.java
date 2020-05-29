31c28f300c4c1cd61ad90ad5bf1756978cc7330d
dfjung4254 <rmsghk4254@naver.com>
2020-05-29 12:17:08 +0900

    spring boot socket project initialize

---
+++ WebSocketServer/src/test/java/com/DJTechnician/WebSocketServer/WebSocketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DJTechnician.WebSocketServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
